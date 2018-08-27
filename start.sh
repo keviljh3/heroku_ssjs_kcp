834372a0ca4615cb922be30c2f7da49694a628c5
keviljh3 <kevinljh3@hotmail.com>
2018-08-27 14:32:54 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-node /app/html.js
+nohup websockify 0.0.0.0:$PORT 127.0.0.1:$(($PORT+50)) --web webdir > /app/websockify.log 2>&1 &
+nohup ssserver -s 0.0.0.0 -p 80 -k Ss123456 -m aes-256-cfb > /app/ss.log 2>&1 &
